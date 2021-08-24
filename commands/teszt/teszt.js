0af1b5e23932ada63518fec2c65c3f24af3fcf62
Son-of-Fury <80508169+Son-of-Fury@users.noreply.github.com>
2021-08-24 10:09:38 +0200

    Delete commands directory

--- commands/teszt/teszt.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "teszt",
-    category: "teszt kategória",
-    description: "teszt command",
-    run: async (bot, message, args) => {
-
-        message.channel.send("szia")
-    }
-}
